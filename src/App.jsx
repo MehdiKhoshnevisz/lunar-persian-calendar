5a50d4a72570e103b7609967004ba3129871b37e
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-02-14 16:11:48 +0330

    Add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import "./App.css";
-
 function App() {
-  return <h1>Lunar Persian Calendar</h1>;
+  return (
+    <main className="h-screen w-full flex justify-center items-center bg-slate-800">
+      <h1 className="text-6xl text-white font-serif">Lunar Persian Calendar</h1>
+    </main>
+  );
 }
 
 export default App;
