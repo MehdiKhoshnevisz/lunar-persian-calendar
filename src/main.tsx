d5444dd4f85b30090de26d4910217a8e3e0707b8
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-02-24 00:41:08 +0330

    Fix build configs

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.js";
+import App from "./App.tsx";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-if (!rootElement) {
-  throw new Error("Could not find root element");
-}
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
