fdd0cd5942f53a9282df38dc1a99d111ebf72c66
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-02-23 20:39:00 +0330

    Add typescript to project

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <main className="h-screen w-full flex justify-center items-center bg-slate-800">
-      <h1 className="text-6xl text-white font-serif">Lunar Persian Calendar</h1>
-    </main>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <h1 className="text-3xl text-dark font-serif p-4">
+      Lunar Persian Calendar
+    </h1>
+  );
+}
+
+export default App;
