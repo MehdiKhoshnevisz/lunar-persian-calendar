d9a115088cb0867b429963366a96720c3a697b45
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-04-23 13:32:50 +0330

    Add weekday for dayjs

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -1,9 +1,11 @@
 import jalaliday from "jalaliday";
 import dayjs, { Dayjs } from "dayjs";
+import weekday from "dayjs/plugin/weekday";
 
 type DateType = Dayjs;
 
 export const useDate = () => {
+  dayjs.extend(weekday);
   dayjs.extend(jalaliday);
   return (date?: DateType) => dayjs(date).calendar("jalali");
 };
