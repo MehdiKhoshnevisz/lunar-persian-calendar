f02252bf3bd25de867ee68040bc3473c7c563dc4
MehdiKhoshnevisz <mkhnsz1996@gmail.com>
2024-06-14 18:48:39 +0330

    Update configs to able to export into projects

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
-import { resolve } from "node:path";
-
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from "path";
 import dts from "vite-plugin-dts";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsConfigPaths from "vite-tsconfig-paths";
-// import EsLint from "vite-plugin-linter";
-// const { EsLinter, linterPlugin } = EsLint;
-import * as packageJson from "./package.json";
+
 // https://vitejs.dev/config/
 export default defineConfig((configEnv) => ({
   plugins: [
     react(),
+    dts(),
     tsConfigPaths(),
-    // linterPlugin({
-    //   include: ["./src}/**/*.{ts,tsx}"],
-    //   linters: [new EsLinter({ configEnv })],
-    // }),
     dts({
       include: ["src/"],
     }),
   ],
   build: {
     lib: {
-      entry: resolve("src", "App.tsx"),
-      name: "LunarPersianCalendar",
+      entry: path.resolve(__dirname, "src/App.tsx"),
+      name: "lunar-persian-calendar",
       formats: ["es", "umd"],
       fileName: (format) => `lunar-persian-calendar.${format}.js`,
     },
   },
+  rollupOptions: {
+    external: ["react", "react-dom"],
+    output: {
+      globals: {
+        react: "React",
+        "react-dom": "ReactDOM",
+      },
+    },
+  },
+  emptyOutDir: true,
 }));
